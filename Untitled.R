fc167901704a58149e1ad70171e27b6de0c74db5
GiacomoLD97 <74047129+GiacomoLD97@users.noreply.github.com>
2024-06-27 10:32:47 +0200

    Create Untitled.R

---
+++ Untitled.R
@@ -0,0 +1,19 @@
+### 1.1 Calculate modified PMN on raw Acoustic Files #####
+
+### Define functions for PMN calculation #####
+
+### Quality check. Remove sites with poor recordings and visible outliers #####
+
+### Read in raw recordings one minute at a time #####
+
+# Loop through PMN calculation
+
+# Subtract Background Noise
+
+#Smooth
+
+#Save summed PMN and BGNsp
+
+### Collate the PMN values #####
+
+### Export Data #####
