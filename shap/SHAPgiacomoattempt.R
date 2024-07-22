bee597cea0e5ebb67d86084d1b7f0fe8287cf993
GiacomoLD97 <74047129+GiacomoLD97@users.noreply.github.com>
2024-07-22 11:51:05 +0200

    SHAP code

--- shap/SHAPgiacomoattempt.R
+++ shap/SHAPgiacomoattempt.R
@@ -1,8 +1,15 @@
-
+### 1. LOAD PACKAGES ##############################
 #Trying XGBoost Modelling
 library(pacman)
 p_load(xgboost) #the main algorithm
 p_load(dplyr)
+p_load(farff)
+p_load(OpenML)
+p_load(dplyr)
+p_load(ggplot2)
+p_load(SHAPforxgboost)
+
+### 2. LOAD AND PREP DATA ##############################
 
 ModellingData <- read.csv("/Users/giacomodelgado/Documents/GitHub/CostaRica/ModellingDataForFigure2oct2023.csv") # Correct path to correct file
 
@@ -18,6 +25,7 @@ agg_data <- ModellingData %>%
 averagedmodellingdata <- agg_data %>% subset(select = -c(Minute, TimeStamp, SummedPMN, SummedNoise, MicType, Latitude, Longitude)) %>% unique()
 quartiles <- quantile(averagedmodellingdata$MeanPMN, probs = c(0.2, 0.4, 0.6, 0.8))
 
+#In case we want to use categorical response variable
 averagedmodellingdata <- averagedmodellingdata %>%
   mutate(Level = case_when(
     MeanPMN <= quartiles[1] ~ "Low",
@@ -27,10 +35,16 @@ averagedmodellingdata <- averagedmodellingdata %>%
     TRUE ~ "High"
   ))
 
+#In case we want to use binary type variable 
 averagedmodellingdata$Plantationtype <- ifelse(grepl("Plantation", averagedmodellingdata$Type, ignore.case = TRUE), TRUE, FALSE)
 averagedmodellingdata$Pasturetype <- ifelse(grepl("Pasture", averagedmodellingdata$Type, ignore.case = TRUE), TRUE, FALSE)
 averagedmodellingdata$NatRegentype <- ifelse(grepl("Natural_Regeneration", averagedmodellingdata$Type, ignore.case = TRUE), TRUE, FALSE)
 averagedmodellingdata$Reftype <- ifelse(grepl("Reference_Forest", averagedmodellingdata$Type, ignore.case = TRUE), TRUE, FALSE)
+averagedmodellingdata$PSA <- ifelse(grepl("Reference_Forest", averagedmodellingdata$Type, ignore.case = TRUE), FALSE, ifelse(grepl("Pasture", averagedmodellingdata$Type), FALSE, TRUE))
+
+
+
+### 3. XGBOOST FOR SHAP WITH 10-MIN DATA AND BINARY TYPE CATEGORIES ##############################
 
 #Data
 xgpreddata <- averagedmodellingdata[,-c(1, 2, 11)]
@@ -70,16 +84,6 @@ fit_xgb <- xgb.train(
   nrounds = 10000 # early stopping
 )
 
-
-
-#SHAP
-p_load(farff)
-p_load(OpenML)
-p_load(dplyr)
-p_load(ggplot2)
-p_load(SHAPforxgboost)
-
-
 # Step 1: Select some observations
 X <- data.matrix(xgpreddata[sample(nrow(xgpreddata), 1000), x])
 
@@ -98,12 +102,7 @@ for (v in shap.importance(shap, names_only = TRUE)) {
 }
 
 
-
-
-
-
-
-
+### 4. XGBOOST FOR SHAP WITH MINUTE BY MINUTE DATA AND BINARY TYPE CATEGORIES ##############################
 
 #Perhaps we try with non-averaged data, using minute instead of ten-min timebin
 
@@ -179,6 +178,183 @@ for (v in shap.importance(shap2, names_only = TRUE)) {
   print(p)
 }
 
-
+#CONCLUSION?
 #Too noisy, keep using aggregated data
 
+
+### 5. XGBOOST FOR SHAP WITH 10-MIN DATA AND CATEGORICAL TYPE ATTRIBUTE ##############################
+
+
+#Data
+xgpreddata3 <- averagedmodellingdata[,-c(1, 11:15)]
+
+#Define response and features
+x3 <- colnames(xgpreddata3)[-8]
+y3 <- "MeanPMN"
+
+#Random split
+set.seed(1) #always put a seed when doing RF,so that results are reproducible
+ix3 <- sample(nrow(xgpreddata3), (nrow(xgpreddata3)*0.8)) #80% training dataset
+
+xgpreddata3 <- as.data.frame(xgpreddata3)
+dtrain3 <- xgb.DMatrix(data.matrix(xgpreddata3[ix3, x3]),
+                      label = xgpreddata3[ix3, y3])
+dvalid3 <- xgb.DMatrix(data.matrix(xgpreddata3[-ix3, x3]),
+                      label = xgpreddata3[-ix3, y3])
+
+
+params <- list(
+  objective = "reg:squarederror",
+  learning_rate = 0.05,
+  subsample = 0.9,
+  colsample_bynode = 1,
+  reg_lambda = 2,
+  max_depth = 5
+)
+
+watchlist3 = list(train=dtrain3, test=dvalid3)
+
+fit_xgb3 <- xgb.train(
+  params,
+  data = dtrain3,
+  watchlist = watchlist3,
+  early_stopping_rounds = 20,
+  print_every_n = 100,
+  nrounds = 10000 # early stopping
+)
+
+# Step 1: Select some observations
+X3 <- data.matrix(xgpreddata3[sample(nrow(xgpreddata3), 1000), x3])
+
+# Step 2: Crunch SHAP values
+shap3 <- shap.prep(fit_xgb3, X_train = X3)
+
+# Step 3: SHAP importance
+shap.plot.summary(shap3)
+
+# Step 4: Loop over dependence plots in decreasing importance
+for (v in shap.importance(shap3, names_only = TRUE)) {
+  p <- shap.plot.dependence(shap3, v, color_feature = "auto", 
+                            alpha = 0.5, jitter_width = 0.1) +
+    ggtitle(v)
+  print(p)
+}
+
+
+### 6. XGBOOST FOR SHAP WITH 10-MIN DATA AND BINARY VALUES FOR ONLY PASTURE AND REFERENCE ##############################
+
+
+#Data
+xgpreddata4 <- averagedmodellingdata[,-c(1:2, 11:12, 14, 16)]
+colnames(xgpreddata4) <- c("Mean Annual Precipitation", "Enhanced Vegetation Index", "Elevation", "Human Footprint", "Average Canopy Height", "10-minute Timebin", "MeanPMN", "Average Background Noise", "Reference Pasture Site", "Reference Forest Site")
+
+#Define response and features
+x4 <- colnames(xgpreddata4)[-7]
+y4 <- "MeanPMN"
+
+#Random split
+set.seed(1) #always put a seed when doing RF,so that results are reproducible
+ix4 <- sample(nrow(xgpreddata4), (nrow(xgpreddata4)*0.8)) #80% training dataset
+
+xgpreddata4 <- as.data.frame(xgpreddata4)
+dtrain4 <- xgb.DMatrix(data.matrix(xgpreddata4[ix4, x4]),
+                      label = xgpreddata4[ix4, y4])
+dvalid4 <- xgb.DMatrix(data.matrix(xgpreddata4[-ix4, x4]),
+                      label = xgpreddata4[-ix4, y4])
+
+
+params <- list(
+  objective = "reg:squarederror",
+  learning_rate = 0.05,
+  subsample = 0.9,
+  colsample_bynode = 1,
+  reg_lambda = 2,
+  max_depth = 5
+)
+
+watchlist4 = list(train=dtrain4, test=dvalid4)
+
+fit_xgb4 <- xgb.train(
+  params,
+  data = dtrain4,
+  watchlist = watchlist4,
+  early_stopping_rounds = 20,
+  print_every_n = 100,
+  nrounds = 10000 # early stopping
+)
+
+# Step 1: Select some observations
+X4 <- data.matrix(xgpreddata4[sample(nrow(xgpreddata4), 1000), x4])
+
+# Step 2: Crunch SHAP values
+shap4 <- shap.prep(fit_xgb4, X_train = X4)
+
+# Step 3: SHAP importance
+shap.plot.summary(shap4)
+
+# Step 4: Loop over dependence plots in decreasing importance
+for (v in shap.importance(shap4, names_only = TRUE)) {
+  p <- shap.plot.dependence(shap4, v, color_feature = "auto", 
+                            alpha = 0.5, jitter_width = 0.1) +
+    ggtitle(v)
+  print(p)
+}
+
+
+### 7. XGBOOST FOR SHAP WITH 10-MIN DATA AND BINARY VALUES FOR ONLY PASTURE, REFERENCE AND PSA ##############################
+
+
+#Data
+xgpreddata5 <- averagedmodellingdata[,-c(1:2, 11:12, 14)]
+
+#Define response and features
+x5 <- colnames(xgpreddata5)[-7]
+y5 <- "MeanPMN"
+
+#Random split
+set.seed(1) #always put a seed when doing RF,so that results are reproducible
+ix5 <- sample(nrow(xgpreddata5), (nrow(xgpreddata5)*0.8)) #80% training dataset
+
+xgpreddata5 <- as.data.frame(xgpreddata5)
+dtrain5 <- xgb.DMatrix(data.matrix(xgpreddata5[ix5, x5]),
+                       label = xgpreddata5[ix5, y5])
+dvalid5 <- xgb.DMatrix(data.matrix(xgpreddata5[-ix5, x5]),
+                       label = xgpreddata5[-ix5, y5])
+
+
+params <- list(
+  objective = "reg:squarederror",
+  learning_rate = 0.05,
+  subsample = 0.9,
+  colsample_bynode = 1,
+  reg_lambda = 2,
+  max_depth = 5
+)
+
+watchlist5 = list(train=dtrain5, test=dvalid5)
+
+fit_xgb5 <- xgb.train(
+  params,
+  data = dtrain5,
+  watchlist = watchlist5,
+  early_stopping_rounds = 20,
+  print_every_n = 100,
+  nrounds = 10000 # early stopping
+)
+
+# Step 1: Select some observations
+X5 <- data.matrix(xgpreddata5[sample(nrow(xgpreddata5), 1000), x5])
+
+# Step 2: Crunch SHAP values
+shap5 <- shap.prep(fit_xgb5, X_train = X5)
+
+# Step 3: SHAP importance
+shap.plot.summary(shap5)
+
+# Step 4: Loop over dependence plots in decreasing importance
+for (v in shap.importance(shap5, names_only = TRUE)) {
+  p <- shap.plot.dependence(shap5, v, color_feature = "auto", 
+                            alpha = 0.5, jitter_width = 0.1) +
+    ggtitle(v)
+  print(p)
+}
